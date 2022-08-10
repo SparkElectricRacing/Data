bc54fbbda8c6d047cb0fb745b630f5f9606a3aa1
cdhardin <cdhardin@umich.edu>
2022-08-10 17:50:28 -0400

    Added new data on 001 and 002, not able to test

--- main.py
+++ main.py
@@ -9,27 +9,43 @@ def process_data(data):
     return bytes
 
 if __name__ == '__main__':
-    logfile = open("REV_test.log")
-
+    logfile = open("peter_pegging.log")
+    #cansend can0
+    
     times = []
-    bms_times = []
-    pack_inst = []
 
+    #001
+    bms_times = []
+    pack_current = []
+    pack_inst_V = []
+    failsafe_status = []
+    high_temp = []
+    low_temp = []
+
+    #002
+    high_cell_V = []
+    low_cell_V = []
+    pack_DCL = []
+    contactor_op = []
+    flags_DTC_1 = []
+
+    #181
     mc181_times = []
     statusword = []
     velocity = []
-
-    #cansend can0
-
+    
+    #281
     mc281_times = []
     motor_temps = []
     mc_temps = []
     warnings = []
     DCL_cir_volts = []
 
+    #381
     mc381_times = []
     throttle_voltage = []
 
+    #581
     mc581_times = []
     rpm = []
     rpm_scaled = []
@@ -53,8 +69,22 @@ if __name__ == '__main__':
                 print(bytes)
                 bms_times.append(time-times[0])
 
-                pack_inst.append(((data >> 16) & 0xFFFF)*0.1)
-                #print("amps: " + str((((bytes[5] << 8) + bytes[4]) - 32767) * 0.1))
+                pack_current.append(((bytes[0] << 8) + bytes[1]))
+                pack_inst_V.append(((bytes[2] << 8) + bytes[3]))
+                # failsafe_status.append(((bytes[4] << 8) + bytes[5]))
+                # high_temp.append(bytes[6])
+                # low_temp.append(bytes[7])
+
+
+            
+            if can_id == '002':
+                print(message)
+                print(bytes)
+                high_cell_V.append(((bytes[0] << 8) + bytes[1]))
+                low_cell_V.append(((bytes[2] << 8) + bytes[3]))
+                pack_DCL.append(bytes[4])
+                contactor_op.append(bytes[5])
+                flags_DTC_1.append(((bytes[6] << 8) + bytes[7]))
 
             if can_id == '181':
                 #print(message)
@@ -63,8 +93,6 @@ if __name__ == '__main__':
                 statusword.append(((bytes[4] << 8) + bytes[5]))
                 #print(((bytes[0] << 32) + (bytes[1] << 16) + (bytes[2] << 8) + bytes[3]))
                 velocity.append(((bytes[0] << 32) + (bytes[1] << 16) + (bytes[2] << 8) + bytes[3]))
-
-
 
 
             if can_id == '281':
@@ -80,26 +108,26 @@ if __name__ == '__main__':
                 DCL_cir_volts.append((((bytes[2] << 8) + bytes[3]))*0.1)
 
 
+            if can_id == '381':
 
-        if can_id == '381':
+                #print(message)
+                #print(bytes)
+                if len(bytes) > 5:
+                    mc381_times.append(time - times[0])
+                    throttle_voltage.append(((bytes[4] << 8) + bytes[5]) * 0.05)
 
-            #print(message)
-            #print(bytes)
-            if len(bytes) > 5:
-                mc381_times.append(time - times[0])
-                throttle_voltage.append(((bytes[4] << 8) + bytes[5]) * 0.05)
 
-        if can_id == '581':
-            mc581_times.append(time - times[1])
+            if can_id == '581':
+                mc581_times.append(time - times[1])
 
-            #print(message)
-            #print(bytes)
+                #print(message)
+                #print(bytes)
 
-            inst_rpm = ((bytes[0] << 32) + (bytes[1] << 16) + (bytes[2] << 8) + bytes[3])
-            rpm.append(inst_rpm)
+                inst_rpm = ((bytes[0] << 32) + (bytes[1] << 16) + (bytes[2] << 8) + bytes[3])
+                rpm.append(inst_rpm)
 
-            scale = 0.05
-            rpm_scaled.append(inst_rpm * scale)
+                scale = 0.05
+                rpm_scaled.append(inst_rpm * scale)
 
 
 
@@ -118,8 +146,12 @@ if __name__ == '__main__':
     plt.plot(mc281_times, warnings, label='Warnings', color='r')
     plt.plot(mc281_times, motor_temps, label='Motor Temp', color='purple')
     plt.plot(mc281_times, DCL_cir_volts, label='DC Link Circuit Voltage', color='pink')
-    plt.plot(bms_times, pack_inst, label='Pack Inst Voltage')
+    plt.plot(bms_times, pack_inst_V, label='Pack Inst Voltage')
     #plt.plot(mc181_times, velocity, label='Velocity', color='r')
+
+
+  
+
 
 
     print('Warnings: ' + str(set(warnings)))
@@ -130,3 +162,20 @@ if __name__ == '__main__':
     plt.legend()
     plt.show()
     print(len(times))
+
+
+
+    #new additions test graph
+    plt.title('new additions test')
+    plt.plot(pack_current, bms_times)
+    plt.plot(pack_inst_V, bms_times)
+    plt.plot(failsafe_status, bms_times)
+    plt.plot(high_temp, bms_times)
+    plt.plot(low_temp, bms_times)
+    plt.plot(high_cell_V, bms_times)
+    plt.plot(low_cell_V, bms_times)
+    plt.plot(pack_DCL, bms_times)
+    plt.plot(contactor_op, bms_times)
+    plt.plot(flags_DTC_1, bms_times)
+    plt.show()
+
